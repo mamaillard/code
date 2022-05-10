7b121e9b61e8bb9525df01aba19d7ac6b7666c8f
mamaillard <105271600+mamaillard@users.noreply.github.com>
2022-05-10 13:29:46 +0200

    Add files via upload

---
+++ sinprime.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import random
+import math
+import numpy as np
+import streamlit as st
+st.write("Demo sineprime")
+N=st.slider("frequency",1,20,1)
+x=np.linspace(0,50,1000)
+y=np.cos(x*N) + np.cos(x*2*N)
+fig, ax=plt.subplots()
+plt.plot(x, y)
+st.pyplot(fig)
+
+
